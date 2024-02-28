167997ae44a4f77c258460b9482d8f61a99f39cd
kimaita <johnnieqaym@gmail.com>
2024-02-28 16:45:14 +0300

    codestyle fix

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree in post-order traversal
  *
  * Does nothing  if tree or func is NULL
  *
