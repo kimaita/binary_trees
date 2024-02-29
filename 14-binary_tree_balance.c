4c85f390d0b2aa743e49053942139d929904e140
kimaita <johnnieqaym@gmail.com>
2024-02-29 17:02:55 +0300

    added task #14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Description: Balance factor is calculated as the difference between
+ * the heights of the left and right subtrees of a node.
+ *
+ * Return: tree's balance factor or 0 if tree NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l_height, r_height;
+
+	if (!tree)
+		return (0);
+
+	/**
+	 * Account for the root node since in the recursion
+	 * we start from the children
+	 */
+	l_height = (tree->left) ? 1 : 0;
+	r_height = (tree->right) ? 1 : 0;
+
+	l_height += binary_tree_height(tree->left);
+	r_height += binary_tree_height(tree->right);
+
+	return (l_height - r_height);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: height or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height, max;
+
+	if (!tree)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	max = (left_height >= right_height) ? left_height : right_height;
+
+	/* Skip incrementing height if at leaf */
+	if (max == 0 && tree->left == NULL && tree->right == NULL)
+		return (max);
+
+	return (max + 1);
+}
